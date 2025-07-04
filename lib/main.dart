23fbf6611c259b52d2605ec5e9347e6ca5e9cd15
JedEstor <145850657+JedEstor@users.noreply.github.com>
2025-07-04 23:06:37 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
   runApp(const RosarioApp());
@@ -14,7 +17,13 @@ class RosarioApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Rosario Hotel & Resorts',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const WelcomeScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WelcomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/profile': (context) => const ProfileScreen(),
+      },
     );
   }
 }
